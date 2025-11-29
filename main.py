456903b4c2373e523ff163a40412e2c8d00497b1
kgiorgio <qwerty46@hotmail.it>
2025-11-29 16:58:40 +0000

    CH4 L5 Kill asteroids but not splitting them yet

--- main.py
+++ main.py
@@ -45,6 +45,14 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+                    
+
         screen.fill("black")
 
         for obj in drawable:
